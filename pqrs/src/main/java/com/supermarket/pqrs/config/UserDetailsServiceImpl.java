7d6a924668b6263de8376349e2e951bcab449ce0
Hugo Latorre <hugoa.latorrep@konradlorenz.edu.co>
2025-05-11 15:44:49 -0500

    Seguridad y Autenticación con Spring Security

---
+++ pqrs/src/main/java/com/supermarket/pqrs/config/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.supermarket.pqrs.config;
+
+import com.supermarket.pqrs.model.Usuario;
+import com.supermarket.pqrs.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        return new User(
+                usuario.getUsername(),
+                usuario.getPassword(),
+                usuario.getRoles().stream()
+                        .map(rol -> new SimpleGrantedAuthority("ROLE_" + rol.getNombre()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
