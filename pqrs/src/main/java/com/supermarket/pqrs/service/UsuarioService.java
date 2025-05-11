579afb99df206a52e04e36c9ab18d722cf608453
Hugo Latorre <hugoa.latorrep@konradlorenz.edu.co>
2025-05-11 15:29:40 -0500

    CRUD completo entidad Usuario

---
+++ pqrs/src/main/java/com/supermarket/pqrs/service/UsuarioService.java
@@ -0,0 +1,32 @@
+package com.supermarket.pqrs.service;
+
+import com.supermarket.pqrs.model.Usuario;
+import com.supermarket.pqrs.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> findById(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void delete(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
