14ca7d3630c70515a6e1ac8c52f8404c6f30f08a
Hugo Latorre <hugoa.latorrep@konradlorenz.edu.co>
2025-05-28 22:58:22 -0500

    implementación home en app.js

--- pqrs-frontend/src/App.js
+++ pqrs-frontend/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Navbar />
       <div className="container mt-4">
         <Routes>
+          <Route path="/home" element={<Home />} />
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
